a838e1e337ec9765288a628ca18a35f190f07dd3
Rodrigo Pinto <rodrigopqn@gmail.com>
2010-09-13 01:11:03 -0300

    modifications to needs of the DevinRio

--- config.ru
+++ config.ru
@@ -1,3 +1,25 @@
 require 'raffleboard'
 
+
+
+configure :development, :production do
+
+  def extract_names
+    workbook = Spreadsheet::ParseExcel.parse('public/people.xls')
+    sheet = workbook.worksheet(0)
+    i=1
+    names = []
+    sheet.each do
+      names << sheet.cell(i,3).to_s('UTF8')
+      i += 1
+    end
+    names
+  end
+
+  set :names, extract_names
+
+end
+
+
 run Sinatra::Application
+
--- raffleboard.rb
+++ raffleboard.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'parseexcel'
 
 get '/' do
   @prizes = []
@@ -8,10 +10,20 @@ get '/' do
     @prizes << {:filename => filename,
                 :name => filename.split('.').first.gsub('_', ' ') }
   end
-  @prize_options = @prizes.map do |prize| 
+  @prize_options = @prizes.map do |prize|
     "<option value=\"#{prize[:filename]}\">#{prize[:name]}</option>"
   end
-  
+
   haml :index
+end
+
+
+get "/sortear" do
+  names = options.names
+  indice = rand(names.length)
+  winner = "#{names[indice]}"
+  options.names.delete_at(indice)
+
+  winner
 end
 
