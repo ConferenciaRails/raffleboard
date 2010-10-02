06939bb6a29e2043d2cc11b50f60af48c3b6a4d0
RamonPage <ramonpage@gmail.com>
2010-10-02 16:29:49 -0300

    Fix raffle load

--- raffleboard.rb
+++ raffleboard.rb
@@ -3,6 +3,23 @@ require 'sinatra'
 require 'haml'
 require 'parseexcel'
 
+def extract_names
+  workbook = Spreadsheet::ParseExcel.parse('public/invitations.xls')
+  sheet = workbook.worksheet(0)
+  i=1
+  names = []
+  sheet.each do
+    name = sheet.cell(i,3).to_s('UTF8').split(' ').map {|w| w.capitalize }.join(' ')
+    status = sheet.cell(i,4).to_s('UTF8')
+
+    names << name if status == "Pago"
+    i += 1
+  end
+  names
+end
+
+set :names, extract_names
+
 get '/' do
   @prizes = []
   Dir.glob 'public/images/sponsors/*' do |filename|
